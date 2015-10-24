4f28beaf3206ecaffea16861ceb556db9768f36f
Daryn Arakawa <daryn.arakawa@gmail.com>
2015-10-23 23:01:30 -0700

    execute-command completed

--- cs111_lab1/lab1-Skeleton/execute-command.c
+++ cs111_lab1/lab1-Skeleton/execute-command.c
@@ -11,6 +11,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 int
 command_status (command_t c)
@@ -27,13 +28,13 @@ void handle_IO(command_t c) {
         int input_fd;
         input_fd = open(c->input, O_RDONLY, 0666);
         if (input_fd < 0) {
-            fprintf(stderr, "Error in opening output file!");
+            fprintf(stderr, "%s: Error opening input file!\n", c->input);
             exit(1);
         }
         
         int dup_result = dup2(input_fd, 0);
         if (dup_result < 0) {
-            fprintf(stderr, "Error in dup2() for input!");
+            fprintf(stderr, "Error in dup2() for input %s\n", c->input);
             exit(1);
         }
         
@@ -46,13 +47,13 @@ void handle_IO(command_t c) {
         output_fd = open(c->output, O_CREAT | O_WRONLY | O_TRUNC, 0666);
         
         if (output_fd < 0) {
-            fprintf(stderr, "Error in opening output file!");
+            fprintf(stderr, "%s : Error opening output file!\n", c->output);
             exit(1);
         }
         
         int dup_result = dup2(output_fd, 1);
         if (dup_result < 0) {
-            fprintf(stderr, "Error in dup2() for output!");
+            fprintf(stderr, "Error in dup2() for output %s\n", c->output);
             exit(1);
         }
         
@@ -61,12 +62,15 @@ void handle_IO(command_t c) {
     
 }
 
+
+
 //what is time_travel?
 void
 execute_command (command_t c, int time_travel)
 {
     
-    pid_t pid;
+    pid_t pid = 200;
+    int fildes[2];
     
     switch (c->type) {
             
@@ -81,22 +85,151 @@ execute_command (command_t c, int time_travel)
             
             else if (pid == 0) { //we are in the child process; execute simple command here
                 
+                
                 handle_IO(c);
+                
                 execvp(c->u.word[0], c->u.word);
-                
-                
+                fprintf(stderr, "%s: command not found\n", c->u.word[0]);
+                exit(1);
             }
             
-            else {   //this is the parent process
-                
-                //do nothing
+            
+            else {  //this is the parent
                 int status;
-                waitpid(pid, &status, WUNTRACED);
-                c->status = status;
-                printf("the command %s exited with code %d", c->u.word[0], c->status);
+                //wait for child to exit
+                while (-1 == waitpid(pid, &status, 0)){
+                    
+                }
+                // printf("Child has not exited yet! WIFEXITED returns %d\n", WIFEXITED(status));
+                //printf("WIFEXITED returns %d\n", WIFEXITED(status));
+                //if (WIFEXITED(status)) {
+                // printf("first child exited with %u\n", WEXITSTATUS(status));
+                if (WIFEXITED(status))
+                    c->status = WEXITSTATUS(status);
+                //printf("Exit status for %s command: %d", c->u.word[0], c->status);
+                
             }
             
+            break;
+        case AND_COMMAND:
             
+            //execute first command in array
+            execute_command(c->u.command[0], time_travel);
+            c->status = c->u.command[0]->status;
+            
+            //execute second command in array if first one exits 0 (i.e. true)
+            if (c->status == 0){
+                execute_command(c->u.command[1], time_travel);
+                c->status = c->u.command[1]->status;
+                
+            }
+            break;
+            
+        case OR_COMMAND:
+            //execute first command in array
+            execute_command(c->u.command[0], time_travel);
+            c->status = c->u.command[0]->status;
+            
+            //if first command isn't true, check second one
+            if (c->status != 0){
+                execute_command(c->u.command[1], time_travel);
+                c->status = c->u.command[1]->status;
+            }
+            
+            break;
+        case SEQUENCE_COMMAND:
+            //recursively call both commands
+            execute_command(c->u.command[0], time_travel);
+            //c->status = c->u.command[0]->status;
+            
+            execute_command(c->u.command[1], time_travel);
+            c->status = c->u.command[1]->status;
+            
+            break;
+        case PIPE_COMMAND:
+            /*
+             int pipe(int fildes[2]);
+             
+             The pipe() function shall create a pipe and place two file descriptors, one each into the arguments fildes[0] and fildes[1], that refer to the open file descriptions for the read and write ends of the pipe.
+             
+             Upon successful completion, 0 shall be returned; otherwise, -1 shall be returned and errno set to indicate the error.
+             */
+            
+            //make a pipe, check for successful creation
+            if (pipe(fildes) == -1){
+                fprintf(stderr, "Cannot create pipe.");
+                exit(1);
+            }
+            
+            pid = fork();
+            
+            
+            if (pid == -1) { //error in fork()
+                fprintf(stderr, "Error in fork() for PIPE_COMMAND!");
+                exit(1);
+            } else if (pid == 0) { //child
+                
+                //close the READ portion (first element), then go on to check WRITE element
+                close(fildes[0]);
+                
+                /*
+                 dup2 to check to see if we can write to pipe
+                 remember: file descriptors have the following integer values:
+                 0: for standard input
+                 1: for standard output
+                 2: for standard error
+                 */
+                
+                if (dup2(fildes[1],1) == -1){
+                    
+                    fprintf(stderr, "Cannot write to pipe");
+                    exit(1);
+                }
+                
+                execute_command(c->u.command[0], time_travel);
+                c->status = c->u.command[0]->status;
+                
+                close(fildes[1]);
+                
+                exit(0);
+                
+            } else if (pid > 0) { //parent
+                
+                int status;
+                
+                //wait for child to ext
+                while (-1 == waitpid(pid, &status, 0)){}
+                
+                //close the WRITE portion
+                close(fildes[1]);
+                
+                //check to see if we can use fildes[0] as input
+                if (dup2(fildes[0],0) == -1){
+                    fprintf(stderr, "dup2() for parent failed");
+                    exit(1);
+                }
+                
+                execute_command(c->u.command[1], time_travel);
+                c->status = c->u.command[1]->status;
+                
+                close(fildes[0]);
+                
+            } else {    //error
+                fprintf(stderr, "Couldn't create child process (PIPE).");
+                exit(1);
+            }
+            
+            break;
+            
+        case SUBSHELL_COMMAND:
+            
+            execute_command(c->u.subshell_command, time_travel);
+            
+            break;
+            
+        default:
+            fprintf(stderr, "command is somehow invalid");
+            exit(1);
             break;
             
             
